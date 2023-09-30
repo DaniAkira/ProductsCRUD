b803173d0c285dd7ec3375770141cac7cbfafe61
Daniel Akira <daakira@hotmail.com>
2023-09-29 23:57:58 -0300

    Adicionando rota para cadastro de produto

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,12 +1,38 @@
-const router = require('express').Router();
-const Products = require('../models/Product');
+const router = require("express").Router();
+const Products = require("../models/Product");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const allRecords = await Products.find();
+
+    res.status(200).json(allRecords);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, price, size, stockAmount, brand, type, sale } = req.body;
+
+  if(!name || !price || brand) {
+    res.status(422).json({error: `Nome, preço e marca são obrigatórios para cadastro de produto.`});
+  }else {
+    const product = {
+        name,
+        price,
+        size,
+        stockAmount,
+        brand,
+        type,
+        sale,
+    };
+
     try {
-        const allRecords = await Products.find();
+        await Products.create(product);
 
-        res.status(200).json(allRecords);
+        res.status(201).json({mensagem: `Produto registrado com sucesso!`}),json(product);
     } catch (error) {
-        res.status(500).json({error: error})
+        res.status(500).json({ error: error });
     }
-})
+  }
+});
