9c568398f74f29b3c0e384b2ae4f927a9b6aa65c
Daniel Akira <daakira@hotmail.com>
2023-10-02 17:32:19 -0300

    Adicionando rota de filtro para shorts

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -161,9 +161,26 @@ router.get('/filter/tshirts', async (req, res) => {
     
     const tshirtProducts = await Products.find(query, toReturn).exec();
     if(tshirtProducts.length === 0) {
-      res.status(200).json({message: `Não existem produtos em promoção.`})
+      res.status(200).json({message: `Não foram encontradas camisetas.`})
     } else {
       res.status(200).json(tshirtProducts);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error });
+        return
+  }
+});
+
+router.get('/filter/shorts', async (req, res) => {
+  try {
+    const query = {type: "Shorts"};
+    const toReturn = {_id: 1, name: 1, price: 1, stockAmount: 1, type: 1}
+    
+    const shortsProducts = await Products.find(query, toReturn).exec();
+    if(shortsProducts.length === 0) {
+      res.status(200).json({message: `Não foram encontrados Shorts.`})
+    } else {
+      res.status(200).json(shortsProducts);
     }
   } catch (error) {
     res.status(500).json({ error: error });
